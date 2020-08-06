0e82adaf0e1131b368bc192da041e0399cfc496c
samantaProspero <samantaprospero@gmail.com>
2020-08-06 09:51:09 -0300

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,89 +1,89 @@
 function addIngredient() {
-    const ingredients = document.querySelector("#ingredients")
-    console.log("ingredients:", ingredients)
-    const fieldContainer = document.querySelectorAll(".ingredient")
-    console.log("fieldContainer:", fieldContainer)
-  
-    // Realiza um clone do último ingrediente adicionado
-    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
-  
-    // Não adiciona um novo input se o último tem um valor vazio
-    if (newField.children[0].value == "") return false
-  
-    // Deixa o valor do input vazio
-    newField.children[0].value = ""
-    ingredients.appendChild(newField)
+  const ingredients = document.querySelector("#ingredients")
+  console.log("ingredients:", ingredients)
+  const fieldContainer = document.querySelectorAll(".ingredient")
+  console.log("fieldContainer:", fieldContainer)
+
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false
+
+  // Deixa o valor do input vazio
+  newField.children[0].value = ""
+  ingredients.appendChild(newField)
 }
 document.querySelector(".add-ingredient").addEventListener("click", addIngredient)
 
 function addPreparation() {
-    const preparations = document.querySelector("#preparations")
-    const fieldContainer = document.querySelectorAll(".preparation")
-    // Realiza um clone do último ingrediente adicionado
-    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
-    // Não adiciona um novo input se o último tem um valor vazio
-    if (newField.children[0].value == "") return false;
-    // Deixa o valor do input vazio
-    newField.children[0].value = ""
-    preparations.appendChild(newField)
+  const preparations = document.querySelector("#preparations")
+  const fieldContainer = document.querySelectorAll(".preparation")
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false;
+  // Deixa o valor do input vazio
+  newField.children[0].value = ""
+  preparations.appendChild(newField)
 }
-document.querySelector(".add-preparation").addEventListener("click",addPreparation)
+document.querySelector(".add-preparation").addEventListener("click", addPreparation)
 
 
 //Paginação
 function paginate(selectedPage, totalPages) {
-    let pages = [],
-        oldPage
+  let pages = [],
+    oldPage
 
-    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
-        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
-        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
 
 
-        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
-            if (oldPage && currentPage - oldPage > 2) {
-                pages.push("...")
-            }
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...")
+      }
 
-            if (oldPage && currentPage - oldPage == 2) {
-                pages.push(oldPage + 1)
-            }
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1)
+      }
 
-            pages.push(currentPage)
+      pages.push(currentPage)
 
-            oldPage = currentPage
-        }
-       
+      oldPage = currentPage
     }
-    return pages
+
+  }
+  return pages
 }
 
 function createPagination(pagination) {
 
-    const filter = pagination.dataset.filter
-    const page = Number(pagination.dataset.page)
-    const total = Number(pagination.dataset.total)
-    const pages = paginate(page, total) 
+  const filter = pagination.dataset.filter
+  const page = Number(pagination.dataset.page)
+  const total = Number(pagination.dataset.total)
+  const pages = paginate(page, total)
 
-    let elements = ""
+  let elements = ""
 
-    for (let page of pages) {
-        if (String(page).includes("...")) {
-            elements += `<span>${page}</span>`
-        } else {
-            if (filter) {
-                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
-            } else {
-                elements += `<a href="?page=${page}">${page}</a>`
-            }
-        }
+  for (let page of pages) {
+    if (String(page).includes("...")) {
+      elements += `<span>${page}</span>`
+    } else {
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+      } else {
+        elements += `<a href="?page=${page}">${page}</a>`
+      }
     }
-    pagination.innerHTML = elements
+  }
+  pagination.innerHTML = elements
 }
 
 const pagination = document.querySelector(".pagination")
-if(pagination){
-    createPagination(pagination)
+if (pagination) {
+  createPagination(pagination)
 }
